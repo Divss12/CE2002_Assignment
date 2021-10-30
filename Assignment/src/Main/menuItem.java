e688e65694906401011521063e66c650af375d92
divss12 <divyanshrbhutra@gmail.com>
2021-10-30 11:10:37 +0800

    finished coding menuItem class, promotional set package not implemented

--- Assignment/src/Main/menuItem.java
+++ Assignment/src/Main/menuItem.java
@@ -14,5 +14,36 @@ public class menuItem {
         this.name = name;
     }
 
+    public void setType(int type){
+        this.type = type;
+    }
+
+    public void setPrice(int price){
+        this.price = price;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public int getType(){
+        return this.type;
+    }
+
+    public int getPrice(){
+        return this.price;
+    }
+    
+    public String getDescription(){
+        return this.description;
+    }
+
+    public String getName(){
+        return this.name;
+    }
     
 }

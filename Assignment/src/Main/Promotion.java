1f61777cae9a50096e7b357e1f3146aa23d39f2e
Encuva <zavierchan1999@gmail.com>
2021-11-05 00:47:07 +0800

    Updated mainApp.java

--- Assignment/src/Main/Promotion.java
+++ Assignment/src/Main/Promotion.java
@@ -1,16 +1,57 @@
 package Main;
 import java.util.ArrayList;
+import java.util.Scanner;
 
-public class Promotion extends MenuItem{
-    private ArrayList<MenuItem> array;
+public class Promotion{
+    protected ArrayList<MenuItem> array;
+	private String name;
+	private String description;
+	private double price;
 
+    
     public Promotion(String n, String d, double p) {
-        super(n, d, p);
+        this.name = n;
+        this.description = d;
+        this.price = p;
         this.array = new ArrayList<MenuItem>();
     }
+    
+    public String getName() {
+    	return this.name;
+    }
 
-    public addItemToPromotion(MenuItem item){
+    public void addItemToPromotion(MenuItem item){
         this.array.add(item);
     }
 
+    public void updateInfo() {
+    	Scanner sc = new Scanner(System.in);
+    	System.out.println("[1] Update item name");
+    	System.out.println("[2] Update item description");
+    	System.out.println("[3] Update item price\nPress any button to exit without updating");
+    	int c = sc.nextInt();
+    	switch(c) {
+    	case 1:
+    		System.out.println("Enter new item name:");
+    		String n = sc.nextLine();
+    		this.name = n;
+
+    		break;
+    	case 2:
+    		System.out.println("Enter new item description:");
+    		String d = sc.nextLine();
+    		this.description = d;
+    		System.out.println("Item description updated.");
+    		break;
+    	case 3:
+    		System.out.println("Enter new item description:");
+    		double p = sc.nextDouble();
+    		this.price = p;
+    		System.out.println("Item price updated.");
+    		break;
+    	default:
+    		System.out.println("Cancelling update...");
+    		break;
+    	}
+    }
 }
